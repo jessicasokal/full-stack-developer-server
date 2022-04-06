c0e450bba6818579df5bc1e0f373d0e802699c0b
jessicasokal <sokal.j@northeastern.edu>
2022-04-06 10:54:07 -0400

    move app listen

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-import helloController
-    from "./controllers/hello-controller.js";
 import userController from "./controllers/user-controller.js";
 import tuitsController from "./controllers/tuits-controller.js";
-
-app.listen(process.env.PORT || 4000);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.listen(process.env.PORT || 4000);
 
-//helloController(app);
 userController(app);
 tuitsController(app);
 //app.get('/hello', (req, res) => {res.send('Hello World!')})
