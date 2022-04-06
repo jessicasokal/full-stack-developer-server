894b804ffd8ca8303db784dfd753f9a9a87b1f1c
jessicasokal <sokal.j@northeastern.edu>
2022-04-06 10:41:24 -0400

    first commit

---
+++ controllers/hello-controller.js
@@ -0,0 +1,7 @@
+const helloController = (app) => {
+    app.get('/hello', (req, res) => {
+        res.send('Hello World!')
+    });
+}
+
+export default helloController;
---
+++ controllers/tuits-controller.js
@@ -0,0 +1,40 @@
+import posts from "./tuits/tuits.js";
+let tuits = posts;
+
+const createTuit = (req, res) => {
+    const newTuit = req.body;
+    newTuit._id = (new Date()).getTime()+'';
+    newTuit.likes = 0;
+    newTuit.dislikes = 0;
+    newTuit.postedBy.username = 'jessica';
+    newTuit.avatar_image = './tesla.jpeg';
+    newTuit.handle = 'jessica';
+    tuits.push(newTuit);
+    res.json(newTuit);
+}
+
+const findAllTuits = (req, res) => {
+    res.json(tuits);
+}
+
+const updateTuit = (req, res) => {
+    const tuitdIdToUpdate = req.params.tid;
+    const updatedTuit = req.body;
+    tuits = tuits.map(t => t._id === tuitdIdToUpdate ? updatedTuit : t);
+    res.sendStatus(200);
+}
+
+const deleteTuit = (req, res) => {
+    const tuitdIdToDelete = req.params.tid;
+    tuits = tuits.filter(t => t._id !== tuitdIdToDelete);
+    res.sendStatus(200);
+}
+
+const tuitsController = (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findAllTuits);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
+
+export default tuitsController;
